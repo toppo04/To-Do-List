fcc3f82d1872e1d95fede6b9e0aeed0a356326bb
ANSHU TOPPO <105590074+toppo04@users.noreply.github.com>
2024-03-14 00:20:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const item = document.querySelector("#item")
+const todobox= document.querySelector("#to-do-box")
+
+item.addEventListener(
+    "keyup",
+    function(event){
+        if(event.key ==  "Enter"){
+            addTOdo(this.value)
+            this.value = ""
+        }
+    }
+) 
+
+const addTOdo = (item) =>{
+    const listitem = document.createElement("li");
+    listitem.innerHTML = `
+    ${item}
+    <i class="fa-solid fa-xmark"></i>
+    `;
+
+    listitem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done");
+        }
+    ) 
+
+    listitem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listitem.remove()
+        }
+    )
+    todobox.appendChild(listitem)
+}
+
